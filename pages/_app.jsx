4bab84f3ba8f6b50f66f2ada788817c4d77bdd11
zey6666 <adharvng298@gmail.com>
2025-03-16 14:45:13 +0530

    _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -37,14 +37,14 @@ export default function AwardApp({ Component, pageProps }) {
       name: "Support",
       icon: "fab fa-discord",
       activeIcon: "fab fa-discord",
-      href: "https://discord.gg/ACJQzJuckW",
+      href: "https://discord.gg/k7n7YAY2JU",
     },
     {
       link: true,
       name: "Add Bot",
       icon: "fal fa-robot",
       activeIcon: "fab fa-robot",
-      href: "https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=bot%20applications.commands",
+      href: "https://discord.com/oauth2/authorize?client_id=1347826530604285974",
     },
     {
       link: true,
