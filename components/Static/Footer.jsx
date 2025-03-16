5374101332f0216c386c1d0c9a2025b6e1598839
zey6666 <adharvng298@gmail.com>
2025-03-16 14:01:35 +0530

    Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -11,7 +11,7 @@ export default function Footer() {
                     <div className="lg:grid lg:grid-cols-6 gap-20">
                         <div className="col-span-3">
                             <div className="flex items-center space-x-5">
-                                <img src="" className="rounded-full w-12" />
+                                <img src=".img/icons8-ghost.gif" className="rounded-full w-12" />
                                 <p className="font-semibold text-xl text-white">GMUSIC</p>
                             </div>
                             <Menu as="div" className="relative mt-3 inline-block text-left">
