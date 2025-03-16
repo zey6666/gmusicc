575c6ba6a69f9c6cd4f9c7473e8b51522381abfe
zey6666 <adharvng298@gmail.com>
2025-03-16 14:41:57 +0530

    index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -52,7 +52,7 @@ export default function Index() {
               <div>
                 <div className="text-white">
                   <p className="text-xl font-semibold mt-5"><i className={`fa fa-cogs text-2xl text-amber-500`} /> Optimization</p>
-                  <p className="text-gray-500 line-clamp-4">With fast and customizable settings specific to your server, MusicMaker gives you a musical pleasure to the fullest.</p>
+                  <p className="text-gray-500 line-clamp-4">With fast and customizable settings specific to your server, GMUSIC gives you a musical pleasure to the fullest.</p>
                 </div>
               </div>
               <div>
@@ -64,7 +64,7 @@ export default function Index() {
               <div>
                 <div className="text-white">
                   <p className="text-xl font-semibold mt-5"><i className={`fa fa-shield text-2xl text-amber-500`} /> Security</p>
-                  <p className="text-gray-500 line-clamp-4">An open source safe bot that you can browse and help us develop.</p>
+                  <p className="text-gray-500 line-clamp-4">100%.</p>
                 </div>
               </div>
               <div>
