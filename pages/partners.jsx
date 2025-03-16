a22b1688a18a4b295adb4f855e3649e57962d8b8
zey6666 <adharvng298@gmail.com>
2025-03-16 15:07:15 +0530

    partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -18,12 +18,12 @@ export default function Partners() {
             <div className="flex flex-col justify-center text-white rounded">
             <a href="https://codeshare.me">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://images-ext-1.discordapp.net/external/YYXCkfsbbSARvGEzBx8W_3gt7ITp4f8YB_2y14SpoiA/%3Fsize%3D4096/https/cdn.discordapp.com/icons/676124992919830548/256b9dd9a6109b1c85ca64677f84f80a.png?format=webp&quality=lossless&width=160&height=160" />
+                            <img className="rounded-full h-32 w-32" src="https://media.discordapp.net/attachments/1242113972262670336/1350764411853213737/wlogo.jpg?ex=67d7ece3&is=67d69b63&hm=96027e64eb43cecf20a3ebc34b9fceefb2dcbfde6bddad896c3133cfaaf55acd&=&format=webp&width=216&height=216" />
                             <div>
-                                <h1 className="leading-none text-3xl font-bold text-white">Code Share</h1>        
+                                <h1 className="leading-none text-3xl font-bold text-white">Whatif Studios</h1>        
                                 <div className="flex items-center mt-1">
                                     <p className="font-normal font-sm">
-                                      Code Share, it offers an ideal platform for developers to share their projects, interact with other developers and sell their projects for money, exploring many projects in different categories and software languages.
+                                      🎥 Welcome to WhatIf Studios!At WhatIf Studios, we bring your favorite movies to life through epic fan-made trailers and creative reimaginings. From Bollywood to Kollywood, Mollywood to Tollywood — we cover movies across India's diverse film industries.
                                     </p>
                                 </div>     
                             </div>
