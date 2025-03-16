507ddb4873a1c42408a81bf03c20ccbbaf199889
zey6666 <adharvng298@gmail.com>
2025-03-16 13:42:06 +0530

    index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ export default function Index() {
     <>
       <div className="py-20 mb-30 max-w-3xl mx-auto">
         <div className="flex w-full justify-center">
-      <img width="500" src="/img/bck.png" />
+      <img width="500" src="/img/icons8-ghost.gif" />
       </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
           GMUSIC
