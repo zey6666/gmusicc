89b1f2149604d49c7f5a4120bb293225267c5a70
zey6666 <adharvng298@gmail.com>
2025-03-16 14:33:18 +0530

    privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -5,9 +5,7 @@ export default function Privacy() {
             <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for MusicMaker</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the GMUSIC bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
-        <div className="py-3"></div>
-        <p>  <a className="text-white" href=""></a> </p>
+        <p>⊳ To use the GMUSIC bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
         <div className="py-3"></div>
         <p>⊳ Every user can use the GMUASIC bot however they want, as long as they do not intend to harm.</p>
         <div className="py-3"></div>
