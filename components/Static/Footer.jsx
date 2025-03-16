f84c5876991135b035363c683106742de47ab819
zey6666 <adharvng298@gmail.com>
2025-03-16 13:57:34 +0530

    Footer.jsx

--- components/Static/Footer.jsx
+++ components/Static/Footer.jsx
@@ -11,8 +11,8 @@ export default function Footer() {
                     <div className="lg:grid lg:grid-cols-6 gap-20">
                         <div className="col-span-3">
                             <div className="flex items-center space-x-5">
-                                <img src="/img/logo2.png" className="rounded-full w-12" />
-                                <p className="font-semibold text-xl text-white">MusicMaker</p>
+                                <img src="/img/logoo.png" className="rounded-full w-12" />
+                                <p className="font-semibold text-xl text-white">GMUSIC</p>
                             </div>
                             <Menu as="div" className="relative mt-3 inline-block text-left">
                                 <div>
@@ -97,7 +97,7 @@ export default function Footer() {
                     </div>
                     <div className="mt-10 grid content-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
                         <p className="text-white text-center sm:text-left text-opacity-50">
-                            {new Date().getFullYear()} &copy; MusicMaker
+                            {new Date().getFullYear()} &copy; GMUSIC
                         </p>
                         <div className="hidden md:flex items-center justify-center">
                                 <p className={"text-xs text-green-400"}>
@@ -105,7 +105,7 @@ export default function Footer() {
                                 </p>
                         </div>
                         <p className="text-white text-center sm:text-right text-opacity-50">
-                            {"Powered by Code Share ❤️"}
+                            {"Powered GAMER EXE"}
                         </p>
                     </div>
                 </div>
