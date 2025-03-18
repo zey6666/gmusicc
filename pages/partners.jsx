a06252e6e849eb826680131fcbe13678b32fbe95
zey6666 <adharvng298@gmail.com>
2025-03-18 20:23:20 +0530

    partners.jsx

--- pages/partners.jsx
+++ pages/partners.jsx
@@ -16,7 +16,7 @@ export default function Partners() {
             <div className="lg:max-w-screen-lg mt-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
 
             <div className="flex flex-col justify-center text-white rounded">
-            <a href="https://www.youtube.com/@WhatIFStudiosOffical">
+            <a href="https://www.youtube.com/@gam3rexe">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
                             <img className="rounded-full h-32 w-32" src="/img/giphy_1.gif" />
                             <div>
