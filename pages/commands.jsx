ba7f63fbfd2b5dd43e1511a493c62836f8adeee6
zey6666 <adharvng298@gmail.com>
2025-03-20 17:56:39 +0530

    commands.jsx

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -88,7 +88,7 @@ Invite GMUSIC</a>
 
   <div className="py-10"></div>
   
-  <Link href={"https://discord.gg/k7n7YAY2JU"}>
+  <Link href={"https://discord.gg/NBMqVNWcwe"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
 Support Server</a>
   </Link>
