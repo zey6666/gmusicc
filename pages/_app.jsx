b0ca99251b26df8d5ba559e38454ce8b72aab26f
zey6666 <adharvng298@gmail.com>
2025-03-20 17:55:46 +0530

    _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -37,7 +37,7 @@ export default function AwardApp({ Component, pageProps }) {
       name: "Support",
       icon: "fab fa-discord",
       activeIcon: "fab fa-discord",
-      href: "https://discord.gg/k7n7YAY2JU",
+      href: "https://discord.gg/NBMqVNWcwe",
     },
     {
       link: true,
